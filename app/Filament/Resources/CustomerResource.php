118e2b4e7d27737c0b1d4c5702e1257d5b9e55ee
sayedsaddam <48250591+sayedsaddam@users.noreply.github.com>
2023-09-19 17:28:19 +0500

    chore: removed the delete action in customer resource.

--- app/Filament/Resources/CustomerResource.php
+++ app/Filament/Resources/CustomerResource.php
@@ -125,7 +125,6 @@ class CustomerResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
                 Tables\Actions\ViewAction::make(),
             ])
             ->bulkActions([
